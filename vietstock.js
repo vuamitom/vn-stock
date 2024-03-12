26d49d50c13e407c0a9064a9fb13952325bd485d
Tam Vu <tamvu89@gmail.com>
2024-03-12 21:29:52 +0700

    login success

--- vietstock.js
+++ vietstock.js
@@ -1,22 +1,25 @@
 import puppeteer from "puppeteer";
+import dotenv from "dotenv";
+
+dotenv.config();
 const chromePath =
   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
-const login = async (page) => {
+const login = async (page, email, password) => {
+  console.info("loggin in as ", email);
   page.goto("https://finance.vietstock.vn/", { timeout: 60000 });
   const loginBtn = await page.waitForSelector("#btn-request-call-login");
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
   await loginBtn.evaluate((btn) => btn.click());
   await page.waitForSelector("#content-login-form-input");
-  await page.type(
-    '#content-login-form-input input[name="Email"]',
-    process.env.USER
-  );
-  await page.type("#txtPassword", process.env.PWD);
+  await page.type('#content-login-form-input input[name="Email"]', email);
+  await page.type("#txtPassword", password);
 
   const triggerLoginBtn = await page.waitForSelector("#btnLoginAccount");
   await triggerLoginBtn.evaluate((btn) => btn.click());
+  await page.waitForSelector("#btnAccountInfo");
+  console.info("logged in successfully");
 };
 
 (async () => {
@@ -29,5 +32,5 @@ const login = async (page) => {
   const page = await browser.newPage();
   page.setDefaultTimeout(5000);
   await page.setViewport({ width: 1280, height: 1024 });
-  await login(page);
+  await login(page, process.env.EMAIL, process.env.PSSWD);
 })();
